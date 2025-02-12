5ee6f11600cb986bc00a71161cfa91d56d10f913
montanher <gabriel.montanherr@gmail.com>
2025-02-12 02:15:44 -0300

    final de curso cypress

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,6 +1,6 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
   beforeEach(() => {
-    cy.visit('./src/index.html')
+    cy.visite(); // Usa o comando personalizado
   })
   it('verifica o titulo da aplicação', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
@@ -24,16 +24,42 @@ it.only('selecione um produto (blog) por seu indice', () => {
   .should('have.value', 'blog')
 })
 
-it.only('marca o tipo de atendimento "Feedback"', () => {
-   cy.get('input[type="radio"][value="feedback"]')
-    .check()
-    .should('be.checked')
+it.only('marca o tipo de atendimento', () => {
+   cy.get('input[type="radio"]')
+    .each(typeOfService => {
+      cy.wrap(typeOfService)
+        .check()
+        .should('be.checked')
+    })
 })
 
-it.only('marca um meio de contato "Telefone"', () => {
-  cy.get('input[type="radio"][value="phone"]')
-  .check()
+it.only('marca ambos checkboxes, depois desmarca o último', () => {
+  cy.get('input[type="checkbox"').check()
   .should('be.checked')
+  .last()
+  .uncheck()
+  .should("not.be.checked")
 })
 
+it.only('Seleciona um arquivo da pasta fixtures', () => {
+  cy.get('#file-upload')
+  .selectFile('cypress/fixtures/example.json')
+  .should(input => {
+      expect(input[0].files[0].name).to.equal('example.json')
+  })
+})
+
+/// teste mais avançados com o sypress.
+
+it('exibe a mensagem por 3 segundos', function () {
+  cy.clock()
+
+  // (...) // ação que dispara algo que exibe uma mensagem por três segundos.
+
+  // (...) // verificação que a mensagem está visível
+
+  cy.tick(3000) // avança o relógio treês segundos (em milissegundos).
+
+  // (...) // verificação de que a mensagem não está mais vísivel.
+})
 })
